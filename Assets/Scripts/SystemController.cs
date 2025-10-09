91d3725974e7e436e7e150ddac15a61c4842586d
ouchan <kingbook0628@gmail.com>
2025-10-09 12:03:23 +0900

    バグ修正

--- Assets/Scripts/SystemController.cs
+++ Assets/Scripts/SystemController.cs
@@ -16,7 +16,7 @@ public class SystemController : MonoBehaviour
     [SerializeField] private Button durationUpgradeButton;
     [SerializeField] private PanSpawner spawner;
     [SerializeField] private int xamount = 100;
-    private int coins = 0;//所持金
+    public int coins = 0;//所持金
     void Awake()
     {
         Instance = this;
@@ -51,8 +51,8 @@ public class SystemController : MonoBehaviour
 
     public void AddCoins(int amount)
     {
-        panInfo.price += amount;
-        coins = panInfo.price;
+        panInfo.price = coins;
+        coins += amount;
         if (coinText != null)
             coinText.text = coins.ToString() + "円";
     }
