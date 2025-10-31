8a4ad43197b9336869dfbb829a23ab0158fa97fc
ouchan <kingbook0628@gmail.com>
2025-10-31 22:55:09 +0900

    BGMManagerとSEManagerがごっちゃになっている

--- Assets/Scripts/SEManager.cs
+++ Assets/Scripts/SEManager.cs
@@ -6,48 +6,55 @@ using UnityEngine.UI;
 public class SEManager : MonoBehaviour
 {
     public static SEManager Instance;
-    [SerializeField] private AudioSource bgmSource;  // カメラ用
-    [SerializeField] private AudioSource sfxSource;  // 効果音用
-
     [SerializeField] private Button onoffButton;
-    [SerializeField] private AudioClip clickClip;
+    [SerializeField] private AudioClip panClickClip;
     [SerializeField] private AudioClip clipon;
     [SerializeField] private AudioClip clipoff;
+
+    [SerializeField] private AudioSource audioSource;
     private bool isOn = false;
     void Awake()
     {
-        if (Instance != null && Instance != this)
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            audioSource = GetComponent<AudioSource>();
+        }
+        else
         {
             Destroy(gameObject);
-            return;
         }
-        Instance = this;
-        DontDestroyOnLoad(gameObject); // 必要なければ削除して可
     }
     void Start()
     {
         //音を流したいけど音源が短いからループで流す
-        bgmSource.loop = true;
-        bgmSource.Play();
+        audioSource.loop = true;
+        audioSource.Play();
         if (onoffButton != null)
             onoffButton.onClick.AddListener(() =>
             {
                 isOn = !isOn;
                 if (isOn)
-                    sfxSource.PlayOneShot(clipon);
+                {
+                    isOn1();
+                }
                 else
-                    sfxSource.PlayOneShot(clipoff);
+                {
+                    isOff1();
+                }
             });
     }
 
     public void PlaySE(AudioClip clip)
     {
-        sfxSource.PlayOneShot(clip);
+        audioSource.PlayOneShot(clip);
     }
     //パンをクリックした時の音　clickはSimpleLineMover.csから呼ばれる
-    public void click() => PlaySE(clickClip);
+    public void click() => PlaySE(panClickClip);
     //Onの時の音 このスクリプトから呼ばれる
-    public void isOn() => PlaySE(clipon);
+    public void isOn1() => PlaySE(clipon);
     //Offの時の音 このスクリプトから呼ばれる
-    public void isOff() => PlaySE(clipoff);
+    public void isOff1() => PlaySE(clipoff);
 }
