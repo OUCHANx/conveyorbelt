a9a9b8b86d82f3ee40ace943bfe1d5040656e286
ouchan <kingbook0628@gmail.com>
2025-11-17 21:40:10 +0900

    hpゲージの追加

---
+++ Assets/Scripts/2nd/HealthGauge.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class HealthGauge : MonoBehaviour
+{
+    [SerializeField] private Image healthImage;
+    public float duration = 0.5f; //０.５秒でヘルスが減るアニメーションを実行
+    public float debugDamageRate = 0.2f;
+    public float currentRate = 1.0f;
+    public void Start()
+    {
+        SetGauge(1.0f);
+    }
+    public void SetGauge(float targetRate)
+    {
+        healthImage.DOFillAmount(targetRate, duration);
+        currentRate = targetRate;
+    }
+    public void TakeDamage(float rate)
+    {
+        SetGauge(currentRate - rate);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(debugDamageRate);
+        }
+    }
+}
